d0e8fe39cb3d7d00bda51c5eca485c32b48c987b
Ebby <gaoc@rc.inesa.com>
2019-11-18 16:40:36 +0800

    Initial version
    Signed-off-by: Ebby <gaoc@rc.inesa.com>

---
+++ Day03-ML_Project/case01-deploy/app.py
@@ -0,0 +1,82 @@
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
+# Flask utils
+from flask import Flask, redirect, url_for, request, render_template
+from werkzeug.utils import secure_filename
+from gevent.pywsgi import WSGIServer
+
+# Define a flask app
+app = Flask(__name__)
+
+# Model saved with Keras model.save()
+# MODEL_PATH = 'models/your_model.h5'
+MODEL_PATH = 'models/base_mode_0.90.h5'
+class_names = {1:'狗', 0:'猫'}
+
+# Load your trained model
+model = load_model(MODEL_PATH)
+# model._make_predict_function()          # Necessary
+print('Model loaded. Start serving...')
+
+
+def model_predict(img_path, model):
+    img = image.load_img(img_path, target_size=(160, 160))
+
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    x = np.expand_dims(x, axis=0)
+
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = x / 255.
+
+    preds = model.predict(x)
+    return preds
+
+
+@app.route('/', methods=['GET'])
+def index():
+    # Main page
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['image']
+
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
+
+        # Make prediction
+        preds = model_predict(file_path, model)
+
+        # Process your result for human
+        #pred_class = preds.argmax(axis=-1)            # Simple argmax
+        result = class_names[0] if preds[0]<0.5 else class_names[1]
+        #result = str(pred_class[0])               # Convert to string
+        return result
+    return None
+
+
+if __name__ == '__main__':
+    # app.run(port=5002, debug=True)
+
+    # Serve the app with gevent
+    http_server = WSGIServer(('0.0.0.0', 5000), app)
+    http_server.serve_forever()
